c04a32e1b80637fc362b06dc66d0716bebb964db
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-16 22:17:13 -0400

    refrsctor

--- src/pages/dogs/FetchYourDog.tsx
+++ src/pages/dogs/FetchYourDog.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useSearch } from '../../services/DogService'
 import { useDebounce } from '@react-hook/debounce';
 import DogSearch from './Search';
@@ -8,9 +7,8 @@ function FetchYourDog() {
     const [query, searchQuery] = useDebounce<string>("", 500)
     const queryResult = useSearch()
 
-    const error = queryResult?.error;
-    const status = queryResult?.status as string;
-    const results = queryResult?.data;
+    const { error, status, data: results } = queryResult || {};
+
     console.log(`queryResult :::`, queryResult, results)
 
     if (error || status === 'error') {
