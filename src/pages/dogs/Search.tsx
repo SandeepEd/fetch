7ccecc43c5a522ce198bbb6a7645c7c888827f35
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-16 21:50:36 -0400

    search component

---
+++ src/pages/dogs/Search.tsx
@@ -0,0 +1,23 @@
+interface ISearchProps {
+    query: string;
+    handleSearch: (query: string) => void;
+}
+const DogSearch: React.FC<ISearchProps> = ({
+    query,
+    handleSearch
+}) => {
+
+    return (
+        <div className="w-screen mt-4">
+            <input
+                className="w-3/5 p-3 place mx-3 border-2 border-gray-300 bg-white h-10 rounded-lg text-sm focus:w-11/12 transition-all"
+                type="text"
+                value={query}
+                onChange={(e) => handleSearch(e.target.value)}
+                placeholder="Search dogs by breed..."
+            />
+        </div>
+    );
+};
+
+export default DogSearch;
