ce33cd1d7635cd40df52d6c9b7237418150daef1
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-16 21:56:10 -0400

    yup Dogs Service

---
+++ src/services/DogService.ts
@@ -0,0 +1,39 @@
+import { QueryObserverResult, useMutation, useQuery } from "react-query";
+import client from "../utils/http";
+
+export class DogService {
+    static getBreeds() {
+        return client.get('/dogs/breeds')
+    }
+
+    static search() {
+        return client.get('/dogs/search')
+    }
+
+    static getDogs(ids: number[]) {
+        return client.post('/dogs', ids)
+    }
+
+
+}
+
+export function useGetBreeds(): QueryObserverResult<Dogs.Base> {
+    return useQuery({
+        queryKey: 'breeds',
+        queryFn: () => DogService.getBreeds().then(res => res.data)
+    })
+}
+
+export function useSearch(): QueryObserverResult<Dogs.ISearch> {
+    return useQuery({
+        queryKey: 'search',
+        queryFn: () => DogService.search().then(res => res.data),
+    })
+}
+
+export function useGetDogs(ids: number[]): QueryObserverResult<Dogs.Base[]> {
+    return useQuery({
+        queryKey: ['dogs'],
+        queryFn: () => DogService.getDogs(ids).then(res => res.data)
+    })
+}
