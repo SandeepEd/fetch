038daedee87a000b531b854e331abf8c653e24b4
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-18 15:21:50 -0400

    more lint changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
-import AppProviders from './providers/AppProviders.tsx';
+import AppProviders from './providers/AppProviders';
 
 ReactDOM.createRoot(document.getElementById(`root`) as HTMLElement).render(
   <React.StrictMode>
