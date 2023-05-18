050643d8773df4b28ac22d319e4db00290275f37
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-17 23:16:53 -0400

    config for queryClient

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -4,7 +4,23 @@ import { ErrorBoundary } from '../container/ErrorBoundary'
 import { AuthContextProvider } from '../context/AuthContext'
 
 function AppProviders({ children }: { children: JSX.Element }) {
-    const queryClient = new QueryClient()
+
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                // cacheTime: 1000 * 60 * 60,
+                refetchOnWindowFocus: false,
+                retry(failureCount: number, error: any) {
+                    if (error.status === 404) { return false; }
+                    else if (failureCount < 2) { return true; }
+                    return false;
+                },
+                // staleTime: 1000 * 60 * 60,
+                useErrorBoundary: true,
+            },
+        },
+    });
+
     return (
         <ErrorBoundary>
             <QueryClientProvider client={queryClient}>
