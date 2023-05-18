9885c33fc6cb4a117b517f74b0f376483bd1f315
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-17 23:16:10 -0400

    Filter component

--- src/pages/dogs/FetchYourDog.tsx
+++ src/pages/dogs/FetchYourDog.tsx
@@ -1,15 +1,25 @@
 import { useSearch } from '../../services/DogService'
-import { useDebounce } from '@react-hook/debounce';
-import DogSearch from './Search';
 import List from './List';
+import FilterComponent from './Filter';
+import { useDogsData } from '../../context/DogsProvider';
+import { useState } from 'react';
 
 function FetchYourDog() {
-    const [query, searchQuery] = useDebounce<string>("", 500)
-    const queryResult = useSearch()
+    const [filter, setFilter] = useState<Dogs.ISearchParams>({})
+    const { breeds } = useDogsData()
+    const queryResult = useSearch({
+        ...filter,
+        size: 10,
+    })
 
-    const { error, status, data: results } = queryResult || {};
+    const handleFilterChange = (newFilter: Dogs.ISearchParams) => {
+        console.log(`newFilter :::`, newFilter)
+        setFilter((prevFil) => ({ ...prevFil, ...newFilter }))
+    }
 
-    console.log(`queryResult :::`, queryResult, results)
+    const { error, status, data: results, isLoading } = queryResult || {};
+
+    // console.log(`queryResult :::`, queryResult, results)
 
     if (error || status === 'error') {
         throw new Error('Error Occurred While Searching for Dogs, Please Try again later!')
@@ -17,13 +27,15 @@ function FetchYourDog() {
 
     return (
         <>
-            <DogSearch
-                query={query}
-                handleSearch={searchQuery}
+            <FilterComponent
+                filter={filter}
+                breeds={breeds}
+                handleFilter={handleFilterChange}
             />
             {
-                status === 'loading' ? <div>Loading...</div> :
-                    results?.resultIds ? <List query={results?.resultIds} /> : <div>no data</div>
+                (status === 'loading' || isLoading) ? <div>Loading...</div> :
+                    results?.resultIds ?
+                        <List query={results?.resultIds} /> : <div>no data</div>
             }
         </>
     )
